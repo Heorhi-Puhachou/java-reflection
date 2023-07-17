3ecdb1ea6aeb1df77dda8f4c7677901a79ef2f78
heorhipuhachou <h.puhachou@gmail.com>
2023-07-17 16:09:30 +0200

    Small update

--- src/ClassAnalyzer.java
+++ src/ClassAnalyzer.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class ClassAnalyzer {
     private static final List<String> JDK_PACKAGE_PREFIXES =
             Arrays.asList("com.sun.", "java", "javax", "jdk", "org.w3c", "org.xml");
@@ -9,10 +10,10 @@ public class ClassAnalyzer {
         /** Complete the Code **/
 
 
-        popupTypeInfo.setPrimitive(/** Complete the Code **/)
-                .setInterface(/** Complete the Code **/)
-                .setEnum(/** Complete the Code **/)
-                .setName(/** Complete the Code **/)
+        popupTypeInfo.setPrimitive(inputClass.isPrimitive())
+                .setInterface(inputClass.isInterface())
+                .setEnum(inputClass.isEnum())
+                .setName(inputClass.getName())
                 .setJdk(isJdkClass(inputClass))
                 .addAllInheritedClassNames(getAllInheritedClassNames(inputClass));
 
@@ -22,16 +23,29 @@ public class ClassAnalyzer {
     /*********** Helper Methods ***************/
 
     public static boolean isJdkClass(Class<?> inputClass) {
-        /** Complete the code
-         Hint: What does inputClass.getPackage() return when the class is a primitive type?
-         **/
+        if (inputClass.isPrimitive()) {
+            return true;
+        }
+        for (String prefix : JDK_PACKAGE_PREFIXES) {
+            if (inputClass.getPackage().toString().startsWith(prefix)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static String[] getAllInheritedClassNames(Class<?> inputClass) {
-        /** Complete the code
-         Hints: What does inputClass.getSuperclass() return when the inputClass doesn't inherit from any class?
-         What does inputClass.getSuperclass() return when the inputClass is a primitve type?
+        List<String> result = new ArrayList<>();
+        if (inputClass.isPrimitive()) {
+            return null;
+        }
+        if (inputClass.isInterface()) {
+            for (Class<?> inh : inputClass.getInterfaces()) {
+                result.add(inh.getName());
+            }
+        }
+        result.add(inputClass.getSuperclass().getName());
 
-         **/
+        return result.toArray(new String[0]);
     }
 }
